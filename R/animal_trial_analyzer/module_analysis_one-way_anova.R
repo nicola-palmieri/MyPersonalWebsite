7efe43345c47a14c32ae36a4ecd37015580d01c1
Nicola Palmieri <97679523+nicola-palmieri@users.noreply.github.com>
2025-10-15 22:02:45 +0200

    Support multi-response ANOVA and visualization

--- R/animal_trial_analyzer/module_analysis_one-way_anova.R
+++ R/animal_trial_analyzer/module_analysis_one-way_anova.R
@@ -13,8 +13,8 @@ one_way_anova_ui <- function(id) {
     br(),
     actionButton(ns("run"), "Run ANOVA"),
     br(), br(),
-    verbatimTextOutput(ns("summary")),
-    DTOutput(ns("fixed_effects"))
+    uiOutput(ns("summary_ui")),
+    uiOutput(ns("fixed_effects_ui"))
   )
 }
 
@@ -34,13 +34,27 @@ one_way_anova_server <- function(id, filtered_data) {
       num_cols <- names(data)[sapply(data, is.numeric)]
       cat_cols <- names(data)[sapply(data, function(x) is.character(x) || is.factor(x))]
       
-      tagList(
+      response_input <- if (isTRUE(input$multi_resp)) {
+        selectizeInput(
+          ns("response"),
+          "Response variables (numeric):",
+          choices = num_cols,
+          selected = if (length(num_cols) > 0) num_cols[1] else NULL,
+          multiple = TRUE,
+          options = list(maxItems = 10)
+        )
+      } else {
         selectInput(
           ns("response"),
           "Response variable (numeric):",
           choices = num_cols,
           selected = if (length(num_cols) > 0) num_cols[1] else NULL
-        ),
+        )
+      }
+
+      tagList(
+        checkboxInput(ns("multi_resp"), "Enable multiple response variables", value = FALSE),
+        response_input,
         selectInput(
           ns("group"),
           "Grouping variable (factor):",
@@ -63,30 +77,117 @@ one_way_anova_server <- function(id, filtered_data) {
       )
     })
     
-    # Fit model
-    model <- eventReactive(input$run, {
-      req(df(), input$response, input$group)
+    # Fit model(s)
+    models <- eventReactive(input$run, {
+      req(df(), input$response, input$group, input$order)
+      responses <- input$response
+      if (!isTRUE(input$multi_resp)) {
+        responses <- responses[1]
+      }
+      responses <- unique(responses)
+      req(length(responses) > 0)
+
+      if (length(responses) > 10) {
+        validate(need(FALSE, "Please select at most 10 response variables."))
+      }
+
       data <- df()
       data[[input$group]] <- factor(data[[input$group]], levels = input$order)
-      model_formula <- as.formula(paste(input$response, "~", input$group))
-      lm(model_formula, data = data)
-    })
-    
-    # Outputs
-    output$summary <- renderPrint({
-      req(model())
-      summary(model())
-    })
-    
-    output$fixed_effects <- renderDT({
-      req(model())
-      datatable(
-        broom::tidy(model()),
-        options = list(scrollX = TRUE, pageLength = 5),
-        rownames = FALSE
+
+      model_list <- list()
+      for (resp in responses) {
+        model_formula <- as.formula(paste(resp, "~", input$group))
+        model_list[[resp]] <- lm(model_formula, data = data)
+      }
+
+      list(
+        models = model_list,
+        responses = responses,
+        factors = list(factor1 = input$group, factor2 = NULL),
+        orders = list(order1 = input$order, order2 = NULL)
       )
     })
-    
-    return(model)
+
+    # Summary outputs
+    output$summary_ui <- renderUI({
+      model_info <- models()
+      if (is.null(model_info)) {
+        return(NULL)
+      }
+
+      responses <- model_info$responses
+
+      if (length(responses) == 1) {
+        verbatimTextOutput(ns("summary_single"))
+      } else {
+        tabs <- lapply(seq_along(responses), function(i) {
+          tabPanel(responses[i], verbatimTextOutput(ns(paste0("summary_", i))))
+        })
+        do.call(tabsetPanel, c(list(id = ns("summary_tabs")), tabs))
+      }
+    })
+
+    # Fixed effects outputs
+    output$fixed_effects_ui <- renderUI({
+      model_info <- models()
+      if (is.null(model_info)) {
+        return(NULL)
+      }
+
+      responses <- model_info$responses
+
+      if (length(responses) == 1) {
+        DTOutput(ns("fixed_effects_single"))
+      } else {
+        tabs <- lapply(seq_along(responses), function(i) {
+          tabPanel(responses[i], DTOutput(ns(paste0("fixed_effects_", i))))
+        })
+        do.call(tabsetPanel, c(list(id = ns("fixed_effects_tabs")), tabs))
+      }
+    })
+
+    observeEvent(models(), {
+      model_info <- models()
+      if (is.null(model_info)) {
+        return()
+      }
+
+      responses <- model_info$responses
+      model_list <- model_info$models
+
+      if (length(responses) == 1) {
+        resp <- responses[1]
+        output$summary_single <- renderPrint({
+          summary(model_list[[resp]])
+        })
+        output$fixed_effects_single <- renderDT({
+          datatable(
+            broom::tidy(model_list[[resp]]),
+            options = list(scrollX = TRUE, pageLength = 5),
+            rownames = FALSE
+          )
+        })
+      } else {
+        for (i in seq_along(responses)) {
+          resp <- responses[i]
+          local({
+            idx <- i
+            response_name <- resp
+            output[[paste0("summary_", idx)]] <- renderPrint({
+              summary(model_list[[response_name]])
+            })
+            output[[paste0("fixed_effects_", idx)]] <- renderDT({
+              datatable(
+                broom::tidy(model_list[[response_name]]),
+                options = list(scrollX = TRUE, pageLength = 5),
+                rownames = FALSE
+              )
+            })
+          })
+        }
+      }
+    })
+
+    return(models)
   })
 }
